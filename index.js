5b50f70f068b42177f507c208386b02aff87c16c
Cara Chew <carachew@gmail.com>
2017-07-14 08:10:00 +0800

    figuring out upload

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+require('dotenv').config({silent: true})
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
+var multer = require('multer');
+var cloudinary = require('cloudinary');
 var app = express();
+var upload = multer({dest: './uploads'});
+
+cloudinary.config({
+  cloud_name: 'smilesandcocktails',
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
@@ -9,4 +19,17 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.listen(3000);
+app.post('/', upload.single('myFile'), function(req, res) {
+  res.send(req.file)
+})
+
+app.post('/', upload.single('myFile'), function(req, res) {
+  cloudinary.upload(req.file.path, function(result) {
+    res.send(result)
+  })
+})
+
+
+
+
+app.listen(8000);
